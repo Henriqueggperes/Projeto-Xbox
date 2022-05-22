bc16e9634a221e389068b4e30538a28ce29c5d79
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-05-22 10:45:22 -0300

    feat(prisma): module&service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { LibModule } from './lib/lib.module';
 import { GenresModule } from './genres/genres.module';
 import { GamesController } from './games/games.controller';
 import { GamesModule } from './games/games.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [LibModule, GenresModule, GamesModule],
+  imports: [LibModule, GenresModule, GamesModule, PrismaModule],
   controllers: [AppController, GamesController],
   providers: [AppService],
 })
