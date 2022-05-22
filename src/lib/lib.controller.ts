75fdb46c7d853fdfb338dfdf795f9562e7d49283
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-05-22 11:47:27 -0300

    feat(lib): implemet 'findOne'

--- src/lib/lib.controller.ts
+++ src/lib/lib.controller.ts
@@ -1,8 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { LibService } from './lib.service';
 import { CreateLibDto } from './dto/create.lib.dto';
 import { Lib } from './entities/lib.entitie';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Library')
 @Controller('lib')
@@ -11,13 +11,26 @@ export class LibController {
   constructor(private readonly libService: LibService){}
 
   @Get()
-  findAll(){
+  @ApiOperation({
+    summary: "Listar todos os jogos da biblioteca"
+  })
+  findAll(): Promise<Lib[]>{
     return this.libService.findAll();
   }
 
+  @Get(':id')
+  @ApiOperation({
+    summary: "Listar um jogo na biblioteca escolhido pelo ID"
+  })
+  findOne(@Param('id') id:string): Promise<Lib> {
+    return this.libService.findOne(id);
+  }
+
 @Post()
-  create(@Body() createShopDto: CreateLibDto){
+@ApiOperation({
+  summary: "Criar um jogo para a biblioteca"
+})
+  create(@Body() createShopDto: CreateLibDto): Promise<Lib>{
     return this.libService.create(createShopDto);
   }
 }
- 
--- src/lib/lib.service.ts
+++ src/lib/lib.service.ts
@@ -8,11 +8,16 @@ export class LibService {
 
   constructor (private readonly prisma: PrismaService){}
 
-  findAll(){
+  findAll(): Promise<Lib[]>{
     return this.prisma.lib.findMany();
   }
 
-  create(createLibDto: CreateLibDto) {
+  findOne(id:string): Promise<Lib>{
+    return this.prisma.lib.findUnique({where:{id}
+    })
+  }
+
+  create(createLibDto: CreateLibDto): Promise<Lib> {
     const Lib: Lib = {...createLibDto};
 
      return this.prisma.lib.create({
