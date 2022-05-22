8e2fd423ac590def444357673f4b1dad50bf1ea1
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-05-22 16:11:05 -0300

    Update update.game.dto.ts

--- src/games/dto/update.game.dto.ts
+++ src/games/dto/update.game.dto.ts
@@ -2,4 +2,13 @@ import { PartialType } from "@nestjs/mapped-types";
 import { CreateGameDto } from "./create.game.dto";
 import { ApiProperty } from "@nestjs/swagger";
 export class UpdateGameDto extends PartialType(CreateGameDto){
+
+  @ApiProperty({
+    example: "Mineirinho Adventures"
+  })
+   gameName: string
+   @ApiProperty({
+     example: 5
+   })
+   gameRate: number
 }
