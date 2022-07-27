f44a210aad45ad962573aefef19cfab1304db9e5
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-07-26 22:16:38 -0300

    Update create.game.dto.ts

--- src/games/dto/create.game.dto.ts
+++ src/games/dto/create.game.dto.ts
@@ -18,6 +18,7 @@ export class CreateGameDto {
   })
   description: string;
 
+  @IsPositive()
   @IsNumber()
   @ApiProperty({
     description: 'Campo onde a entidade Game irá reservar a avaliação do jogo',
