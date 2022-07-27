720143fd05b08507ca6a6b94a82b8cd5e4b681d8
Henrique Gomes Peres <97991367+Henriqueggperes@users.noreply.github.com>
2022-07-27 14:04:56 -0300

    Update update-user.dto.ts

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType,OmitType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends OmitType(CreateUserDto, ['cpf'] as const) {}
+export class UpdateUserDto extends OmitType(CreateUserDto, ['cpf','password','confirmPassword'] as const) {}
